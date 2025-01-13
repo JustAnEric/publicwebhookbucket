875b980f1fcba8e52145b7985e7c9ddacf762cc5
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2025-01-14 09:21:13 +1100

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js');
 
 const bot = new discord.Client();
-const token = "https://discord.com/api/webhooks/1326310215905443891/N0ISX7Ur_oUTvXD94qshJFqoCxk9ShKWUnAlaVXfWe9wU8KINTpcJW7z-PjOMsJjp4pE";
+const token = "https://discord.com/api/webhooks/";
 
 bot.login(token);
